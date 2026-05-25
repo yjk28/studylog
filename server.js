ef62ff8467bde8c9b8207b7028b13b48d0c3eb2c
Jerry Yang <yjk2817@gmail.com>
2026-05-25 14:58:43 +0800

    Upgrade server to serve static files

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
+const PUBLIC = path.join(__dirname, 'public');
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>StudyLog Server Running!</h1>');
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = path.join(PUBLIC, filePath);
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  });
 }).listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
 });
